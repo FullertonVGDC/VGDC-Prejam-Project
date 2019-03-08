6af08a00ec479fc3dc90d15b96dd1fc49d163f33
Michael Perez <mperez980089@yahoo.com>
2019-03-08 01:55:56 -0800

    Added bounce pad, tweaked player values in main scene

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    public float bouncePower = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        other.rigidbody.velocity = new Vector2(other.rigidbody.velocity.x, bouncePower);
+    }
+}
